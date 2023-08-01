1091f0d7055e72646bdbd0d50b54b4105c27cec9
Ali Yamani <aliyamanii@outlook.com>
2023-08-01 15:13:24 +0330

    Added script types

---
+++ project-todo-list/script.ts
@@ -0,0 +1,20 @@
+enum TaskStatus {
+    TODO = "todo",
+    DOING = "doing",
+    DONE = "done",
+  }
+  
+  enum TaskLabel {
+    GREEN = "Green",
+    RED = "Red",
+    BLUE = "Blue",
+    YELLOW = "Yellow",
+  }
+  
+  interface Task {
+    id: number;
+    status: TaskStatus;
+    title: string;
+    labels: TaskLabel[];
+    deadline?: Date;
+  }
