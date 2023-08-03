d9c97d07a2a278f69edd8c661377015d0ec09f35
Ali Yamani <aliyamanii@outlook.com>
2023-08-03 03:51:15 +0330

    Completed html/css

--- project-todo-list/script.js
+++ project-todo-list/script.js
@@ -1,56 +1,101 @@
 var taskList = [];
 function addTask() {
-    var titleInput = document.querySelector(".task-input");
-    var stateInput = document.querySelector(".state-input");
-    var labelInput = document.querySelector(".label-input");
-    var title = titleInput.value.trim();
-    var state = stateInput.value;
-    var label = labelInput.value.trim();
-    if (title === "")
-        return;
-    var newTask = { title: title, state: state, label: label };
-    taskList.push(newTask);
-    renderTasks();
-    clearInputFields();
+  var titleInput = document.querySelector(".task-input");
+  var stateInput = document.querySelector(".state-input");
+  var labelInput = document.querySelector(".label-input");
+  var title = titleInput.value.trim();
+  var state = stateInput.value;
+  var label = labelInput.value.trim();
+  if (title === "") return;
+  var newTask = { title: title, state: state, label: label };
+  taskList.push(newTask);
+  renderTasks();
+  clearInputFields();
 }
 function removeTask(index) {
-    taskList.splice(index, 1);
-    renderTasks();
+  taskList.splice(index, 1);
+  renderTasks();
 }
 function renderTasks() {
-    var taskListElement = document.getElementById("taskList");
-    taskListElement.innerHTML = "";
-    taskList.forEach(function (task, index) {
-        var listItem = document.createElement("li");
-        listItem.innerHTML = "\n        <span>".concat(task.title, "</span>\n        <button class=\"remove-button\" onclick=\"removeTask(").concat(index, ")\">Remove</button>\n        <select class=\"status-dropdown\" onchange=\"changeTaskState(").concat(index, ", this.value)\">\n          <option value=\"todo\"").concat(task.state === "todo" ? " selected" : "", ">Todo</option>\n          <option value=\"doing\"").concat(task.state === "doing" ? " selected" : "", ">Doing</option>\n          <option value=\"done\"").concat(task.state === "done" ? " selected" : "", ">Done</option>\n        </select>\n        <span class=\"label\">").concat(task.label, "</span>\n      ");
-        taskListElement.appendChild(listItem);
-    });
+  var taskListElement = document.getElementById("taskList");
+  taskListElement.innerHTML = "";
+  taskList.forEach(function (task, index) {
+    var listItem = document.createElement("li");
+    listItem.innerHTML = "\n        <span>"
+      .concat(
+        task.title,
+        '</span>\n        <button class="remove-button" onclick="removeTask('
+      )
+      .concat(
+        index,
+        ')">Remove</button>\n        <select class="status-dropdown" onchange="changeTaskState('
+      )
+      .concat(index, ', this.value)">\n          <option value="todo"')
+      .concat(
+        task.state === "todo" ? " selected" : "",
+        '>Todo</option>\n          <option value="doing"'
+      )
+      .concat(
+        task.state === "doing" ? " selected" : "",
+        '>Doing</option>\n          <option value="done"'
+      )
+      .concat(
+        task.state === "done" ? " selected" : "",
+        '>Done</option>\n        </select>\n        <span class="label">'
+      )
+      .concat(task.label, "</span>\n      ");
+    taskListElement.appendChild(listItem);
+  });
 }
 function changeTaskState(index, newState) {
-    taskList[index].state = newState;
+  taskList[index].state = newState;
 }
 function filterTasks() {
-    var filterInput = document.querySelector(".search-input");
-    var filterValue = filterInput.value.trim().toLowerCase();
-    var filteredTasks = taskList.filter(function (task) {
-        return task.title.toLowerCase().includes(filterValue) ||
-            task.state.toLowerCase().includes(filterValue) ||
-            task.label.toLowerCase().includes(filterValue);
-    });
-    var taskListElement = document.getElementById("taskList");
-    taskListElement.innerHTML = "";
-    filteredTasks.forEach(function (task, index) {
-        var listItem = document.createElement("li");
-        listItem.innerHTML = "\n        <span>".concat(task.title, "</span>\n        <button class=\"remove-button\" onclick=\"removeTask(").concat(index, ")\">Remove</button>\n        <select class=\"status-dropdown\" onchange=\"changeTaskState(").concat(index, ", this.value)\">\n          <option value=\"todo\"").concat(task.state === "todo" ? " selected" : "", ">Todo</option>\n          <option value=\"doing\"").concat(task.state === "doing" ? " selected" : "", ">Doing</option>\n          <option value=\"done\"").concat(task.state === "done" ? " selected" : "", ">Done</option>\n        </select>\n        <span class=\"label\">").concat(task.label, "</span>\n      ");
-        taskListElement.appendChild(listItem);
-    });
+  var filterInput = document.querySelector(".search-input");
+  var filterValue = filterInput.value.trim().toLowerCase();
+  var filteredTasks = taskList.filter(function (task) {
+    return (
+      task.title.toLowerCase().includes(filterValue) ||
+      task.state.toLowerCase().includes(filterValue) ||
+      task.label.toLowerCase().includes(filterValue)
+    );
+  });
+  var taskListElement = document.getElementById("taskList");
+  taskListElement.innerHTML = "";
+  filteredTasks.forEach(function (task, index) {
+    var listItem = document.createElement("li");
+    listItem.innerHTML = "\n        <span>"
+      .concat(
+        task.title,
+        '</span>\n        <button class="remove-button" onclick="removeTask('
+      )
+      .concat(
+        index,
+        ')">Remove</button>\n        <select class="status-dropdown" onchange="changeTaskState('
+      )
+      .concat(index, ', this.value)">\n          <option value="todo"')
+      .concat(
+        task.state === "todo" ? " selected" : "",
+        '>Todo</option>\n          <option value="doing"'
+      )
+      .concat(
+        task.state === "doing" ? " selected" : "",
+        '>Doing</option>\n          <option value="done"'
+      )
+      .concat(
+        task.state === "done" ? " selected" : "",
+        '>Done</option>\n        </select>\n        <span class="label">'
+      )
+      .concat(task.label, "</span>\n      ");
+    taskListElement.appendChild(listItem);
+  });
 }
 function clearInputFields() {
-    var titleInput = document.querySelector(".task-input");
-    var labelInput = document.querySelector(".label-input");
-    titleInput.value = "";
-    labelInput.value = "";
+  var titleInput = document.querySelector(".task-input");
+  var labelInput = document.querySelector(".label-input");
+  titleInput.value = "";
+  labelInput.value = "";
 }
 document.addEventListener("DOMContentLoaded", function () {
-    renderTasks();
+  renderTasks();
 });
